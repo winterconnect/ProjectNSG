9794bef8cdb1fdccaf5b89236ae602e25eff0a4f
hijin <hijin1998@gmail.com>
2021-02-12 12:50:43 +0900

    merge project

--- yyApp/urls.py
+++ yyApp/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'yyApp'
 urlpatterns = [
-    path('join/', views.choose_authority),
+    # path('', views.home),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('join/', views.choose_authority, name='join'),
     path('join/adopter/', views.join_adopter),
     path('join/guardian/', views.join_guardian),
-]
+    path('writepost/', views.write_post),
+    path('postdetail/', views.post_detail),
+    # path('postlike/', views.post_like, name='post_like'),
+    path('board/', views.BoardListView.as_view(), name='board_list'),
+    path('', views.home, name='index'),
+    path('pie-chart/', views.pie_chart, name='pie-chart'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- yyApp/views.py
+++ yyApp/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth.hashers import make_password
-from yyApp.models import Member
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.hashers import make_password, check_password
+from yyApp.models import Member, Board, Pet
+from django.core.paginator import Paginator
+from django.db.models import Q, Count
+from django.contrib import messages
+from django.template import context
+from django.views.generic import ListView
+from yyApp.chartData import state, city
+import datetime
 
 
 # Create your views here.
+
+def check_session(request):
+    get_session = request.session.get('user')
+    if get_session:
+        login_member = Member.objects.get(memberID=get_session)
+    else:
+        login_member = ''
+    return login_member
+
+
 def join_adopter(request):
     if request.method == "GET":
         return render(request, 'yyApp/adopter.html')
@@ -18,13 +35,16 @@ def join_adopter(request):
         adopterAddress = request.POST.get('adopterAddress', None)
         adopterFamily = request.POST.get('adopterFamily', None)
         res_data = {}
-        if not (memberID and memberPW and memberName and memberEmail): #and 수정
+        if not (memberID and memberPW and memberName and memberEmail):
             res_data['error'] = '모든 값을 입력해주세요'
         else:
-            member = Member(memberID=memberID, memberPW=make_password(memberPW), memberName=memberName,
-                            memberEmail=memberEmail, memberAge=memberAge, adopterHouse=adopterHouse,
-                            adopterAddress=adopterAddress, adopterFamily=adopterFamily, authority=False)
-            member.save()
+            if memberPW == request.POST['memberPW_check']:
+                member = Member(memberID=memberID, memberPW=make_password(memberPW), memberName=memberName,
+                                memberEmail=memberEmail, memberAge=memberAge, adopterHouse=adopterHouse,
+                                adopterAddress=adopterAddress, adopterFamily=adopterFamily, authority=False)
+                member.save()
+            else:
+                res_data['error'] = '비밀번호와 비밀번호 확인이 일치하지 않아요'
         return render(request, 'yyApp/adopter.html', res_data)
 
 
@@ -42,12 +62,187 @@ def join_guardian(request):
         if not (memberID and memberPW and memberName and memberEmail):
             res_data['error'] = '모든 값을 입력해주세요'
         else:
-            member = Member(memberID=memberID, memberPW=make_password(memberPW), memberName=memberName,
-                            memberEmail=memberEmail, memberAge=memberAge, authority=True)
-            member.save()
+            if memberPW == request.POST['memberPW_check']:
+                member = Member(memberID=memberID, memberPW=make_password(memberPW), memberName=memberName,
+                                memberEmail=memberEmail, memberAge=memberAge, authority=True)
+                member.save()
+            else:
+                res_data['error'] = '비밀번호와 비밀번호 확인이 일치하지 않아요'
+
         return render(request, 'yyApp/guardian.html', res_data)
 
 
 def choose_authority(request):
     if request.method == "GET":
         return render(request, 'yyApp/jointype.html')
+
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'yyApp/login.html')
+    elif request.method == "POST":
+        login_id = request.POST.get('username', None)
+        login_pw = request.POST.get('password', None)
+        res_data = {}
+
+        if not (login_id and login_pw):
+            res_data['error'] = '아이디와 비밀번호 모두 입력해주세요.'
+        else:
+            try:
+                member = Member.objects.get(memberID=login_id)
+                if check_password(login_pw, member.memberPW):
+                    request.session['user'] = member.memberID
+                    return redirect('/yyApp')
+                else:
+                    res_data['error'] = '잘못된 비밀번호입니다.'
+            except Member.DoesNotExist:
+                res_data['error'] = '존재하지 않는 아이디입니다.'
+        return render(request, 'yyApp/login.html', res_data)
+
+
+def logout(request):
+    a = check_session(request)
+    print(a.memberID)
+    if request.session['user']:
+        del (request.session['user'])
+    return redirect('/yyApp')
+
+
+def home(request):
+    return render(request, 'yyApp/index_haedong.html', {'login_member': check_session(request)})
+
+
+def save_post(post):
+    post.save()
+
+
+def write_post(request):
+    # if request.method == "GET":
+    # return render(request, 'yyApp/writepost.html')
+    errors = []
+    if request.method == 'POST':
+        member = check_session(request)
+        title = request.POST.get('title', '').strip()
+        content = request.POST.get('content', '').strip()
+        date = datetime.date.today().isoformat()
+        petName = request.POST.get('petName', '')
+        petBirth = request.POST.get('petBirth', '')
+        petSex = request.POST.get('petSex', '')
+        if petSex == 'Male':
+            petSex = True
+        elif petSex == 'Female':
+            petSex = False
+        else:
+            petSex = None
+        petSize = request.POST.get('petSize', '')
+        petLoc = request.POST.get('petLoc', None)
+        petSpecies = request.POST.get('petSpecies', '')
+        petWeight = float(request.POST.get('petWeight', 0))
+        petNeuter = request.POST.get('petTNR', '')
+        petColor = request.POST.get('petColor', '')
+        petImage = request.FILES['petImage']
+        hashtag = request.POST.get('hashtag', '').split('#')
+
+        if not title:
+            errors.append('제목을 입력하세요.')
+        if not content:
+            errors.append('내용을 입력하세요.')
+        if not errors:
+
+            post = Board(memberID=Member.objects.get(memberID=member.memberID), title=title, content=content,
+                         date=date, hashtag=hashtag)
+
+            save_post(post)
+
+            saved_post = Board.objects.order_by('-id').first()
+
+            pet = Pet(postID=Board.objects.get(id=saved_post.id), petName=petName, petBirth=petBirth, petSex=petSex,
+                      petSize=petSize, petLoc=petLoc, petSpecies=petSpecies, petWeight=petWeight, petImage=petImage,
+                      petNeuter=petNeuter, petColor=petColor, memberID=Member.objects.get(memberID=member.memberID))
+
+            # 두개의 테이블 연동 및 date 자료 인식 불가?
+            pet.save()
+            # for hashtag in hashtag:
+            #     hashtag = hashtag.strip()
+            #     post.hashtag.add(hashtag)
+    return render(request, 'yyApp/writepost.html', {'user': request.user, 'errors': errors})
+
+
+def post_detail(request):
+    post = Board.objects.order_by('-id').first()
+    pet = Pet.objects.get(postID=post.postID)
+    is_check = False
+    try:
+        if str(post.memberID) == request.session['user']:
+            is_check = True
+    except KeyError:
+        is_check = False
+    return render(request, 'yyApp/postdetail.html', {'post': post, 'is_check': is_check, 'pet': pet})
+
+
+class BoardListView(ListView):
+    model = Board
+    paginate_by = 9
+    template_name = 'yyApp/board_list.html'  # DEFAULT : <app_label>/<model_name>_list.html
+    context_object_name = 'board_list'  # DEFAULT : <model_name>_list
+
+    def get_queryset(self):
+        board_list = Board.objects.order_by('-id')
+
+        search_keyword = self.request.GET.get('q', '')
+        search_type = self.request.GET.get('type', '')
+
+        if search_keyword:
+            if len(search_keyword) > 1:
+                if search_type == 'all':
+                    search_board_list = board_list.filter(
+                        Q(title__icontains=search_keyword) | Q(content__icontains=search_keyword))
+                elif search_type == 'title_content':
+                    search_board_list = board_list.filter(
+                        Q(title__icontains=search_keyword) | Q(content__icontains=search_keyword))
+                elif search_type == 'title':
+                    search_board_list = board_list.filter(title__icontains=search_keyword)
+                elif search_type == 'content':
+                    search_board_list = board_list.filter(content__icontains=search_keyword)
+                return search_board_list
+            else:
+                messages.error(self.request, '검색어는 2글자 이상 입력해주세요.')
+        return board_list
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context['paginator']
+        page_numbers_range = 5
+        max_index = len(paginator.page_range)
+
+        page = self.request.GET.get('page')
+        current_page = int(page) if page else 1
+
+        start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
+        end_index = start_index + page_numbers_range
+        if end_index >= max_index:
+            end_index = max_index
+
+        page_range = paginator.page_range[start_index:end_index]
+        context['page_range'] = page_range
+
+        search_keyword = self.request.GET.get('q', '')
+        search_type = self.request.GET.get('type', '')
+        board_fixed = Board.objects.order_by('-id')
+
+        if len(search_keyword) > 1:
+            context['q'] = search_keyword
+        context['type'] = search_type
+        context['board_fixed'] = board_fixed
+
+        return context
+
+
+def pie_chart(request):
+    labels = list(state['상태'])
+    data = list(state['견 수'])
+
+    return JsonResponse(data={
+        'labels': labels,
+        'data': data,
+    })
